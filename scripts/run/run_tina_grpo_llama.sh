e3a67b61a87e1fb1ea85160b7ebf2b4e89d5ebb9
UpUp Ashton Wang <upup.ashton.wang@gmail.com>
2025-10-18 21:56:38 -0700

    scripts for qwen, llama and octothinker

--- scripts/run/run_tina_grpo_llama.sh
+++ scripts/run/run_tina_grpo_llama.sh
@@ -6,33 +6,30 @@ echo "PYTHON ENV: $(which python)"
 source "./scripts/set/set_vars.sh"
 
 export CUDA_VISIBLE_DEVICES=0,1
-#export CUDA_VISIBLE_DEVICES=2,3
-#export CUDA_VISIBLE_DEVICES=4,5,6,7
-#export CUDA_VISIBLE_DEVICES=6,7
 GPU_COUNT=$(python -c "import torch; print(torch.cuda.device_count())")
 
 echo -e "\nNumber of GPUs: ${GPU_COUNT}\n"
 
 # Configuration
-MODEL_SIZE="1.5B"             # Options: 1.5B, 3B, 7B, 14B, 32B
-POST_TRAIN_TYPE="lora"      # Options: full, lora, dora, dora_cache
-USE_QLORA="false"            # Options: true, false
+MODEL_SIZE="8B"             # Options: 8B
+POST_TRAIN_TYPE="full"      # Options: full, lora
+USE_QLORA="false"           # Options: false
 USE_COMPILE="false"         # Options: true, false
 SEED=42                     # Set a seed for reproducibility
 
-export BASE_MODEL_PATH="${CKPT_DIR}/qwen2_5/Qwen2.5-${MODEL_SIZE}"
-MODEL_PATH="Qwen/Qwen2.5-${MODEL_SIZE}"
+export BASE_MODEL_PATH="${CKPT_DIR}/llama3_1/Llama-3.1-${MODEL_SIZE}"
+BASE_MODEL_NAME="meta-llama/Llama-3.1-${MODEL_SIZE}"
 
 # Override config in recipe
-export CONFIG_NAME="${POST_TRAIN_TYPE}_grpo_qwen2_5_${MODEL_SIZE}"
-export BASE_MODEL_DIR="${CKPT_DIR}/qwen2_5"
-export BASE_OUTPUT_DIR="${OUTPUT_DIR}/qwen2_5"
+export CONFIG_NAME="${POST_TRAIN_TYPE}_grpo_llama3_1_${MODEL_SIZE}"
+export BASE_MODEL_DIR="${CKPT_DIR}/llama3_1"
+export BASE_OUTPUT_DIR="${OUTPUT_DIR}/llama3_1"
 export MODEL_SIZE
 
 # Download model if needed
 if [ ! -d "${BASE_MODEL_PATH}" ]; then
     echo "Downloading base model to ${BASE_MODEL_PATH}..."
-    tune download "${MODEL_PATH}" \
+    tune download "${BASE_MODEL_NAME}" \
         --output-dir "${BASE_MODEL_PATH}" \
         --hf-token "${HF_TOKEN}"
 else
@@ -44,13 +41,13 @@ if [ "${POST_TRAIN_TYPE}" == "full" ]; then
     echo "Using full model for post-training."
     echo "Random Seed: ${SEED}"
     tune run --nproc_per_node "${GPU_COUNT}" full_grpo_distributed \
-        --config qwen2_5/"${MODEL_SIZE}_full_grpo" \
+        --config llama3_1/"${MODEL_SIZE}_full_grpo" \
         seed="${SEED}" \
         compile="${USE_COMPILE}"
 else
     # Select config file based on QLoRA usage
     CONFIG_SUFFIX=$( [ "${USE_QLORA}" == "true" ] && echo "qlora_grpo" || echo "lora_grpo" )
-    CONFIG_FILE="qwen2_5/${MODEL_SIZE}_${CONFIG_SUFFIX}"
+    CONFIG_FILE="llama3_1/${MODEL_SIZE}_${CONFIG_SUFFIX}"
 
     # Map POST_TRAIN_TYPE to lora_type (default to "lora" if not specified)
     LORA_TYPE="${POST_TRAIN_TYPE}"
