28eb72f35e1817e6a2fb1afc981c3230de2a8ab6
UpUp Ashton Wang <upup.ashton.wang@gmail.com>
2025-12-02 11:06:08 -0800

    update qwen script

--- scripts/run/run_grpo_qwen.sh
+++ scripts/run/run_grpo_qwen.sh
@@ -6,30 +6,30 @@ echo "PYTHON ENV: $(which python)"
 source "./scripts/set/set_vars.sh"
 
 export CUDA_VISIBLE_DEVICES=0,1
-#export CUDA_VISIBLE_DEVICES=2,3
-#export CUDA_VISIBLE_DEVICES=4,5
-#export CUDA_VISIBLE_DEVICES=6,7
 GPU_COUNT=$(python -c "import torch; print(torch.cuda.device_count())")
 
 echo -e "\nNumber of GPUs: ${GPU_COUNT}\n"
 
 # Configuration
-MODEL_SIZE="3B"             # Options: 1.5B, 3B, 7B, 14B, 32B
+MODEL_SIZE="3B"           # Options: 3B for qwen2_5, 1.7B for qwen3
+MODEL_FAMILY="qwen2_5"        # Options: qwen2_5, qwen3
+temp_string="${MODEL_FAMILY//_/.}"
+MODEL_PREFIX="${temp_string^}"
 POST_TRAIN_TYPE="full"      # Options: full, lora, dora, dora_cache
-USE_QLORA="false"            # Options: true, false
+USE_QLORA="false"           # Options: true, false
 USE_COMPILE="false"         # Options: true, false
 
 # --- MODIFICATION: Define seeds as an array ---
 # Set all the seeds you want to run here
 SEEDS=(42)
 
-export BASE_MODEL_PATH="${CKPT_DIR}/qwen2_5/Qwen2.5-${MODEL_SIZE}"
-MODEL_PATH="Qwen/Qwen2.5-${MODEL_SIZE}"
+export BASE_MODEL_PATH="${CKPT_DIR}/${MODEL_FAMILY}/${MODEL_PREFIX}-${MODEL_SIZE}"
+MODEL_PATH="Qwen/${MODEL_PREFIX}-${MODEL_SIZE}"
 
 # Override config in recipe
-export CONFIG_NAME="${POST_TRAIN_TYPE}_grpo_qwen2_5_${MODEL_SIZE}"
-export BASE_MODEL_DIR="${CKPT_DIR}/qwen2_5"
-export BASE_OUTPUT_DIR="${OUTPUT_DIR}/qwen2_5"
+export CONFIG_NAME="${POST_TRAIN_TYPE}_grpo_${MODEL_FAMILY}_${MODEL_SIZE}"
+export BASE_MODEL_DIR="${CKPT_DIR}/${MODEL_FAMILY}"
+export BASE_OUTPUT_DIR="${OUTPUT_DIR}/${MODEL_FAMILY}"
 export MODEL_SIZE
 
 # Download model if needed (runs only once)
@@ -55,13 +55,13 @@ for SEED in "${SEEDS[@]}"; do
         echo "Using full model for post-training."
         echo "Random Seed: ${SEED}"
         tune run --nproc_per_node "${GPU_COUNT}" full_grpo_distributed \
-            --config qwen2_5/"${MODEL_SIZE}_full_grpo" \
+            --config "${MODEL_FAMILY}/${MODEL_SIZE}_full_grpo" \
             seed="${SEED}" \
             compile="${USE_COMPILE}"
     else
         # Select config file based on QLoRA usage
         CONFIG_SUFFIX=$( [ "${USE_QLORA}" == "true" ] && echo "qlora_grpo" || echo "lora_grpo" )
-        CONFIG_FILE="qwen2_5/${MODEL_SIZE}_${CONFIG_SUFFIX}"
+        CONFIG_FILE="${MODEL_FAMILY}/${MODEL_SIZE}_${CONFIG_SUFFIX}"
 
         # Map POST_TRAIN_TYPE to lora_type (default to "lora" if not specified)
         LORA_TYPE="${POST_TRAIN_TYPE}"
